bd12be32b5a2fab63c55b849a83185218b07a842
Layanhani <145140373+Layanhani@users.noreply.github.com>
2024-04-28 19:43:42 +0300

    Update Department.java

--- stage-3/Company-system/src/main/java/com/mycompany/company/system/Department.java
+++ stage-3/Company-system/src/main/java/com/mycompany/company/system/Department.java
@@ -1,28 +1,63 @@
-//HI
+//LAYAN ALNOUMANI
+
+
+
 package com.mycompany.company.system;
+
+
 
 import java.util.ArrayList;
 
 
+
+
 public class Department {
     
-    public String name;
-    public Address address;
-    public Employee head;
-    ArrayList<Office> offices;
 
-    public Department(String name, Address address, Employee head) {
-        this.name = name;
-        this.address = address;
-        this.head = head;
-        this.offices = new ArrayList<>();
-    }
+
+
+                  public String name;
+                  public Address address;
+                   public Employee head;
+                       ArrayList<Office> offices;
+
     
-    public void addOffice(Office office) {
-        this.offices.add(office);
+              public Department(String name, Address address, Employee head) 
+    {
+  
+                  this.name = name;
+                  this.address = address;
+                  this.head = head;
+                  this.offices = new ArrayList<>();
+  
     }
+
     
-    public void removeOffice(Office office) {
-        this.offices.remove(office);
+
+
+      public void addOffice(Office office) 
+    {
+
+
+           this.offices.add(office);
+
+
+            }
+    
+
+
+        public void removeOffice(Office office)
+    
+    {
+
+
+
+           this.offices.remove(office);
+
+
+
+          }
+
+
+
     }
-}
